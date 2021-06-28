3b40b43c5d50a9db05b1bcb2fea5e79a802d0ba6
SlabyG <gabriel17jh@gmail.com>
2021-06-28 15:36:04 +0000

    added the API and also updated the castle section with the maps, each map will represent the location of each castle. Suport given by tutor Sean from CI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,40 @@
+
+
+/*  Castle*/
 function initMap() {
-    const myLatLng = { lat: 45.750167579106616, lng: 22.88817136444397 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 2,
-      center: myLatLng,
-    });
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "Hello World!",
-    });
-  }
+   // first map
+   const myLatLng1 = { lat: 45.749457311570545, lng: 22.888329538529746 };
+   const map1 = new google.maps.Map(document.getElementById("map1"), {
+    zoom: 17,
+    center: myLatLng1,
+  })
+  new google.maps.Marker({
+    position: myLatLng1,
+    map1,
+    title: "Corvin Castle",
+  });
+  
+   // second map
+   const myLatLng2 = { lat: 45.51497735855289, lng: 25.36720661363668 };
+   const map2 = new google.maps.Map(document.getElementById("map2"), {
+    zoom: 17,
+    center: myLatLng2,
+  })
+  new google.maps.Marker({
+    position: myLatLng2,
+    map2,
+    title: "Bran Castle",
+  });
+  
+   // third map
+   const myLatLng3 = { lat: 45.360117274075456, lng: 25.542631569454663 };
+   const map3 = new google.maps.Map(document.getElementById("map3"), {
+    zoom: 17,
+    center: myLatLng3,
+  })
+  new google.maps.Marker({
+    position: myLatLng3,
+    map3,
+    title: "Peles Castle",
+  });
+}
