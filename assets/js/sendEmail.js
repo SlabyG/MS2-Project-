32d0c12cea6dd190a83dd395dc49097dcbaaf6e3
SlabyG <gabriel17jh@gmail.com>
2021-07-06 17:28:37 +0000

    i have adjusted the buttons so that they can be equal size also finalised the form section and conected it to the navigation, checking to see if form are working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,18 @@
-console.log("hello")
+function sendMail(contactForm){
+    emailjs.sendMail("gmail","slabyg", {
+        "last_name": contactForm.last_name.value,
+        "fist_name": contactForm.fist_name.value,
+        "tour_name": contactForm.tour_name.value,
+        "email": contactForm.email_add.value,
+        "additional_info": contactForm.additional_info.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error){
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
