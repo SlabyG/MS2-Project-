2002d6e46d891bcf1ef1358e2f1b8879cf2b1615
SlabyG <gabriel17jh@gmail.com>
2021-07-08 09:09:09 +0000

    added the favicon as i kept getting errors  in the console also tested tehe form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,20 +1,20 @@
 function sendMail(contactForm){
-    emailjs.sendMail("gmail","slabyg", {
-        "last_name": contactForm.last_name.value,
-        "first_name": contactForm.first_name.value,
-        "tour_name": contactForm.tour_name.value,
-        "email": contactForm.email.value,
-        "additional_info": contactForm.additional_info.value,
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error){
-            console.log("FAILED", error);
-        }
-    );
-    return false;
+  emailjs.sendMail("gmail","slabyg", {
+      "last_name": contactForm.last_name.value,
+      "first_name": contactForm.first_name.value,
+      "tour_name": contactForm.tour_name.value,
+      "email": contactForm.email.value,
+      "additional_info": contactForm.additional_info.value,
+  })
+  .then(
+      function(response) {
+          console.log("SUCCESS", response);
+      },
+      function(error){
+          console.log("FAILED", error);
+      }
+  );
+  return false;
 }
 
 const btn = document.getElementById('button');
@@ -23,17 +23,13 @@ document.getElementById('contact-form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
-
-   const serviceID = 'default_service';
+   const serviceID = 'Slabyg';
    const templateID = 'slabyg';
-   
+
    emailjs.sendForm(serviceID, templateID, this)
-   .then(() => {
-     btn.value = 'Send Email';
-     alert('Sent!');
-   }, (err) => {
-     btn.value = 'Send Email';
-     alert(JSON.stringify(err));
-   });
+    .then(() => {
+      alert('Sent!');
+    }, (err) => {
+      alert(JSON.stringify(err));
+    });
 });
